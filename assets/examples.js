a79e96aea83b8596ba2aa587c8c77c8666974b09
xudafeng <xudafeng@126.com>
2015-07-05 23:40:33 +0800

    tweak animation util

--- assets/examples.js
+++ assets/examples.js
@@ -18755,7 +18755,9 @@
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-	var Util = __webpack_require__(165);
+	var Common = __webpack_require__(159);
+
+	var Util = Common.Util;
 
 	function transitionEnd() {
 	  var el = document.createElement('pin');
@@ -18846,55 +18848,7 @@
 	module.exports = AnimationManager;
 
 /***/ },
-/* 165 */
-/***/ function(module, exports) {
-
-	/* ================================================================
-	 * autoresponsive-react by xdf(xudafeng[at]126.com)
-	 *
-	 * first created at : Mon Jun 02 2014 20:15:51 GMT+0800 (CST)
-	 *
-	 * ================================================================
-	 * Copyright 2014 xdf
-	 *
-	 * Licensed under the MIT License
-	 * You may not use this file except in compliance with the License.
-	 *
-	 * ================================================================ */
-
-	'use strict';
-
-	exports.merge = function (r, s) {
-	  this.each(s, function (v, k) {
-	    r[k] = v;
-	  });
-	  return r;
-	};
-
-	exports.each = function (obj, fn) {
-	  for (var i in obj) {
-	    if (obj.hasOwnProperty(i)) {
-	      fn.call(this, obj[i], i);
-	    }
-	  }
-	  return obj;
-	};
-
-	exports.extend = function () {
-	  var args = Array.prototype.slice.call(arguments);
-	  var object = args.shift();
-
-	  for (var i = 0, l = args.length; i < l; i++) {
-	    var props = args[i];
-
-	    for (var key in props) {
-	      object[key] = props[key];
-	    }
-	  }
-	  return object;
-	};
-
-/***/ },
+/* 165 */,
 /* 166 */
 /***/ function(module, exports) {
 
