d0dde513a828559a34631dbbb6124cf85cbb59a8
xudafeng <xudafeng@126.com>
2022-04-18 00:20:10 +0800

    fix: test case

--- test/homepage.test.js
+++ test/homepage.test.js
@@ -37,7 +37,7 @@ describe('./test/homepage.test.js', () => {
         .quit();
     });
 
-    it.skip('panel should be ok', () => {
+    it('panel should be ok', () => {
       const elementGroup = `#simplest .btn-group`;
       const elementButton = `${elementGroup} button`;
       const elementRect = `document.querySelector('${elementGroup}').getClientRects()`;
@@ -50,45 +50,9 @@ describe('./test/homepage.test.js', () => {
           return Promise.reduce([null].concat(queue), (i, selector) => {
             return driver
               .elementByCss(selector)
-              .click()
-              .sleep(500)
-              .click()
-              .sleep(500);
-          });
-        });
-    });
-
-    it.skip('waterfall should be ok', () => {
-      const element = `#waterfall .rc-autoresponsive-container`;
-      const elementRect = `document.querySelector('${element}').getClientRects()`;
-      const height = `${elementRect}[0].y || ${elementRect}[0].top`;
-
-      return driver
-        .sleep(1500)
-        .execute(`window.scrollTo(0, ${height})`)
-        .sleep(500)
-        .elementsByCss(`${element} .item`)
-        .then(list => {
-          const queue = list.map((item, key) => `${element} > .item:nth-child(${key + 1})`);
-          return Promise.reduce(queue, (i, selector) => {
-            return driver
-              .elementByCss(selector)
-              .click()
-              .sleep(1000)
               .click();
           });
         });
-    });
-
-    it.skip('i18n should be ok', () => {
-      return driver
-        .elementByCss('div.i18n-buttons > div > button:nth-child(2)')
-        .click()
-        .sleep(1000)
-        .elementByCss('div.i18n-buttons > div > button:nth-child(1)')
-        .click()
-        .getUrl(`${BASE_URL}?locale=zh`)
-        .sleep(500);
     });
 
     it('resize should be ok', () => {
