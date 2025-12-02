cdfee53bb4db94c1dd9a22a0b7558731cc348827
达峰的夏天 <xudafeng@126.com>
2025-12-02 17:38:15 +0800

    Add unit tests for AutoResponsive component

---
+++ test/unit/autoresponsive.test.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const React = require('react');
+const { expect } = require('chai');
+const sinon = require('sinon');
+const TestRenderer = require('react-test-renderer');
+
+const AutoResponsive = require('../../src/index');
+
+function createChild(key, overrides = {}) {
+  return React.createElement('div', Object.assign({
+    key,
+    className: 'item',
+    style: {
+      width: '50',
+      height: '60',
+    },
+  }, overrides));
+}
+
+describe('AutoResponsive', () => {
+  it('respects fixed container height and float layout when container width is not set', () => {
+    const onItemDidLayout = sinon.spy();
+    const onContainerDidLayout = sinon.spy();
+
+    const instance = new AutoResponsive(Object.assign({}, AutoResponsive.defaultProps, {
+      containerHeight: 120,
+      itemMargin: 5,
+      children: [createChild('one')],
+      onItemDidLayout,
+      onContainerDidLayout,
+    }));
+
+    instance.sortManager = {
+      changeProps: sinon.spy(),
+      init: sinon.spy(),
+      getPosition: sinon.stub().returns([15, 25]),
+    };
+
+    instance.animationManager = {
+      generate: sinon.stub().callsFake(options => ({
+        top: options.position[1],
+        left: options.position[0],
+      })),
+    };
+
+    const tree = instance.render();
+    const renderer = TestRenderer.create(tree);
+    const container = renderer.root.findByProps({ className: `${instance.props.prefixClassName}-container` });
+    const renderedChild = container.findByType('div');
+
+    expect(instance.fixedContainerHeight).to.equal(true);
+    expect(instance.containerStyle.height).to.equal(120);
+    expect(instance.sortManager.changeProps.calledOnce).to.equal(true);
+    expect(instance.sortManager.init.calledOnce).to.equal(true);
+    expect(onItemDidLayout.calledOnce).to.equal(true);
+    expect(onContainerDidLayout.calledOnce).to.equal(true);
+
+    expect(renderedChild.props.style.float).to.equal('left');
+    expect(renderedChild.props.style.margin).to.equal('0 5px 5px 0');
+    expect(renderedChild.props.style.position).to.equal(undefined);
+    expect(renderedChild.props.style.top).to.equal(25);
+    expect(renderedChild.props.style.left).to.equal(15);
+  });
+
+  it('calculates dynamic container height, filters items, and uses absolute positioning when width is provided', () => {
+    const onItemDidLayout = sinon.spy();
+    const onContainerDidLayout = sinon.spy();
+
+    const instance = new AutoResponsive(Object.assign({}, AutoResponsive.defaultProps, {
+      containerWidth: 200,
+      itemMargin: 10,
+      children: [
+        createChild('skip', { className: 'skip-me' }),
+        createChild('two', { style: { width: '80', height: '70' } }),
+      ],
+      onItemDidLayout,
+      onContainerDidLayout,
+    }));
+
+    const positionStub = sinon.stub();
+    positionStub.onFirstCall().returns([5, 5]);
+
+    instance.sortManager = {
+      changeProps: sinon.spy(),
+      init: sinon.spy(),
+      getPosition: positionStub,
+    };
+
+    instance.animationManager = {
+      generate: sinon.stub().callsFake(options => ({
+        translateX: options.position[0],
+        translateY: options.position[1],
+      })),
+    };
+
+    const tree = instance.render();
+    const renderer = TestRenderer.create(tree);
+    const container = renderer.root.findByProps({ className: `${instance.props.prefixClassName}-container` });
+    const renderedChild = container.findByType('div');
+
+    expect(instance.fixedContainerHeight).to.equal(false);
+    expect(instance.containerStyle.height).to.equal(85);
+    expect(instance.sortManager.changeProps.calledOnce).to.equal(true);
+    expect(instance.sortManager.init.calledOnce).to.equal(true);
+    expect(onItemDidLayout.calledOnce).to.equal(true);
+    expect(onContainerDidLayout.calledOnce).to.equal(true);
+
+    expect(renderedChild.props.style.position).to.equal('absolute');
+    expect(renderedChild.props.style.float).to.equal(undefined);
+    expect(renderedChild.props.style.translateX).to.equal(5);
+    expect(renderedChild.props.style.translateY).to.equal(5);
+  });
+});
